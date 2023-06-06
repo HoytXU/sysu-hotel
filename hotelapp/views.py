7ebae058af0c71bd79632511ca3490ec8eecb157
I-Paradox-I <1834089722@qq.com>
2023-06-06 16:22:40 +0800

    modified:   hotel/__pycache__/urls.cpython-310.pyc 	modified:   hotel/urls.py 	modified:   hotelapp/__pycache__/views.cpython-310.pyc 	modified:   hotelapp/views.py 	new file:   media/Client_images/campus-east.jpg 	new file:   media/Client_images/f3.jpg 	new file:   media/Food_images/logo.jpg 	new file:   media/Food_images/logo_3RB2QUj.jpg 	new file:   media/Food_images/sina.jpg 	new file:   media/Food_images/sunyatsen.jpg 	new file:   media/Room_images/home.gif 	new file:   media/Room_images/home_pLNDkkC.gif 	new file:   media/Staff_images/f2.jpg 	new file:   media/Staff_images/f3.jpg 	new file:   media/Staff_images/tencent.jpg 	new file:   media/Vip_images/f2.jpg 	new file:   media/Vip_images/f2_0UgPeYy.jpg 	new file:   media/Vip_images/f5.jpg 	new file:   media/Vip_images/sunyatsen.jpg 	modified:   templates/accomodation.html 	modified:   templates/client.html 	modified:   templates/food.html 	modified:   templates/order.html 	modified:   templates/park.html 	modified:   templates/room.html 	modified:   templates/staff.html 	modified:   templates/vip.html

--- hotelapp/views.py
+++ hotelapp/views.py
@@ -12,8 +12,7 @@ from hotelapp.models import Accommodation
 
 #定义展示函数
 def index(request):
-    people_list = Staff.objects.all()
-    return render(request, "index.html", {"people_list":people_list})
+    return render(request, "index.html")
 
 #员工插入函数
 def insert_staff(request):
@@ -30,7 +29,9 @@ def insert_staff(request):
             staff.员工相片 = image  # 将图片文件赋值给模型的image字段
 
         staff.save()
-    return render(request, "staff.html")
+    people_list = Staff.objects.all()
+    return render(request, "staff.html", {"people_list":people_list})
+
 #员工信息删除
 def delete_staff(request):
     if request.method == "GET":
@@ -50,11 +51,20 @@ def search_staff(request):
 def update_staff(request):
     if request.method == "POST":
         staff_id = request.POST.get("id")
+        name= request.POST.get("name")
         job = request.POST.get("job")
         salary = request.POST.get("salary")
         time = request.POST.get("time")
+        
         image = request.FILES.get("image", None)  # 获取上传的图片文件
-        Staff.objects.filter(员工id=staff_id).update(职务=job, 月薪=salary, 工龄=time,员工相片=image)
+        staff=Staff.objects.filter(员工id=staff_id).first()
+        staff.员工id=staff_id
+        staff.姓名=name
+        staff.职务=job
+        staff.员工相片=image
+        staff.月薪=salary
+        staff.工龄=time
+        staff.save()
         return render(request, 'staff.html')
 
 #顾客信息录入
@@ -114,15 +124,14 @@ def update_client(request):
 #         clients = eval('Client.objects.filter('+columns+'=' + '"'+value+'"' + ')')
 #     return render(request, 'client.html', {"clients": clients})
 
+#顾客信息查询
 def search_client(request):
     clients = []  # 默认空的查询结果
 
     if request.method == "POST":
         columns = request.POST.get("columns", None)
         value = request.POST.get("value", None)
-
         if columns and value:
-            print(1)
             clients = Client.objects.filter(**{columns: value})
 
     return render(request, 'client.html', {"clients": clients})
@@ -140,14 +149,8 @@ def insert_vip(request):
             vip.会员相片 = image  # 将图片文件赋值给模型的image字段
 
         vip.save()
-    return render(request, 'vip.html')
-
-
-#会员信息展示
-def show_vip(request):
-    people_list = Vip.objects.all()
-    return render(request, "vip.html", {"people_list": people_list})
-
+    vip_list = Vip.objects.all()
+    return render(request, 'vip.html', {"vip_list": vip_list})
 
 #会员信息删除
 def delete_vip(request):
@@ -161,10 +164,17 @@ def delete_vip(request):
 def update_vip(request):
     if request.method == "POST":
         vip_id = request.POST.get("id")
+        name = request.POST.get("name")
         rank = request.POST.get("rank")
         tel = request.POST.get("tel")
         image = request.FILES.get("image", None)  # 获取上传的图片文件
-        Vip.objects.filter(会员编号=vip_id).update(会员等级=rank, 联系方式=tel,顾客相片=image)
+        vip=Vip.objects.filter(会员编号=vip_id).first()
+        vip.姓名=name
+        vip.会员等级=rank
+        vip.会员编号=vip_id
+        vip.联系方式=tel
+        vip.会员相片=image
+        vip.save()
         return render(request, 'vip.html')
 
 
@@ -178,14 +188,7 @@ def search_vip(request):
 
 
 
-#菜品展示函数
-def show_food(request):
-    food_list = Food.objects.all()
-    return render(request, 'staff.html2', {"food_list": food_list})
-
-
-
-#菜品信息录入
+#菜品信息展示/录入
 def insert_food(request):
     if request.method == "POST":
         food_id = request.POST.get("id", None)
@@ -199,13 +202,9 @@ def insert_food(request):
             food.菜品相片 = image  # 将图片文件赋值给模型的image字段
 
         food.save()
-    return render(request, 'food.html')
+    food_list = Food.objects.all()
+    return render(request, 'food.html', {"food_list": food_list})
 
-
-#菜品信息展示
-def show_food(request):
-    people_list = Food.objects.all()
-    return render(request, "food.html", {"people_list": people_list})
 
 #菜品信息删除
 def delete_food(request):
@@ -219,10 +218,18 @@ def delete_food(request):
 def update_food(request):
     if request.method == "POST":
         food_id = request.POST.get("id")
+        name = request.POST.get("name")
         price = request.POST.get("price")
         cooker = request.POST.get("cooker")
+        cook = Staff.objects.filter(职务='厨师').get(员工id=cooker)
         image = request.FILES.get("image", None)  # 获取上传的图片文件
-        Food.objects.filter(菜品编号=food_id).update(价格=price, 厨师id=Staff.objects.filter(职务='厨师').get(员工id=cooker),菜品相片=image)
+        food=Food.objects.filter(菜品编号=food_id).first()
+        food.菜品编号=food_id
+        food.价格=price
+        food.厨师id=cook
+        food.菜名=name
+        food.菜品相片=image
+        food.save()
         return render(request, 'food.html')
 
 
@@ -245,13 +252,8 @@ def insert_park(request):
         print(num)
         twz = Park.objects.create(车位编号=car_id, 当前状态=status, 车辆牌号=num)
         twz.save()
-    return render(request, 'park.html')
-
-
-#停车场信息展示
-def show_park(request):
-    people_list = Park.objects.all()
-    return render(request, "park.html", {"people_list": people_list})
+    car_list = Park.objects.all()
+    return render(request, 'park.html', {"car_list": car_list})
 
 
 #停车场信息删除
@@ -269,7 +271,11 @@ def update_park(request):
         car_id = request.POST.get("id")
         status = request.POST.get("status")
         num = request.POST.get("num")
-        Park.objects.filter(车位编号=car_id).update(当前状态=status, 车辆牌号=num)
+        print(car_id, status, num)
+        car = Park.objects.filter(车位编号=car_id).first()
+        car.当前状态=status
+        car.车辆牌号=num
+        car.save()
         return render(request, 'park.html')
 
 
@@ -296,13 +302,8 @@ def insert_room(request):
             room.客房款式相片 = image  # 将图片文件赋值给模型的image字段
 
         room.save()
-    return render(request, 'room.html')
-
-
-#客房信息展示
-def show_room(request):
-    people_list = Room.objects.all()
-    return render(request, "room.html", {"people_list": people_list})
+    room_list = Room.objects.all()
+    return render(request, "room.html", {"room_list": room_list})
 
 
 #客房信息查询
@@ -331,7 +332,12 @@ def update_room(request):
         price = request.POST.get("price")
         waiter = request.POST.get("waiter")
         image = request.FILES.get("image", None)  # 获取上传的图片文件
-        Room.objects.filter(房间号=room_id).update(客房类型=room_type, 价格=price, 负责人编号=Staff.objects.filter(职务='服务员').get(员工id=waiter),客房款式相片=image)
+        room = Room.objects.filter(房间号=room_id).first()
+        room.客房类型=room_type
+        room.价格=price
+        room.负责人编号=Staff.objects.filter(职务='服务员').get(员工id=waiter)
+        room.客房款式相片=image
+        room.save()
         return render(request, 'room.html')
 
 
@@ -348,13 +354,8 @@ def insert_order(request):
         cook = Staff.objects.filter(职务='服务员').get(员工id=waiter)
         twz = Order.objects.create(id=order_id, 顾客编号=cli, 菜品编号=foodid, 服务员编号=cook, 桌号=table)
         twz.save()
-    return render(request, 'order.html')
-
-
-#菜品订单信息展示
-def show_order(request):
-    people_list = Order.objects.all()
-    return render(request, "order.html", {"people_list": people_list})
+    order_list = Order.objects.all()
+    return render(request, 'order.html', {"order_list": order_list})
 
 
 #菜品订单信息删除
@@ -373,7 +374,7 @@ def search_order(request):
         print(foodid)
         food_id_set = []
         for f in foodid:
-            fid = f.菜品编号
+            fid = f.菜品编号.菜品编号
             food_id_set.append(fid)
         print(food_id_set)
         food_list = []
@@ -387,7 +388,8 @@ def search_order(request):
             }
             food_list.append(li)
         print(food_list)
-    return render(request, 'order.html', {"food_list": food_list})
+    order_list = Order.objects.all()
+    return render(request, 'order.html', {"order_list": order_list, "food_list": food_list})
 
 
 #客房信息录入
@@ -401,51 +403,36 @@ def insert_accomodation(request):
         roomid = Room.objects.get(房间号=room)
         twz = Accommodation.objects.create(id=aid, 顾客id=cli, 客房编号=roomid, 入住时间=time)
         twz.save()
-    return render(request, 'accomodation.html')
+    acc_list = Accommodation.objects.all()   
+    return render(request, 'accomodation.html', {"acc_list": acc_list})
 
 
 #客房订单删除
 def delete_accomodation(request):
     if request.method == "GET":
-        aid = request.GET.get("id")
-        Accommodation.objects.filter(id=aid).delete()
+        cid = request.GET.get("id")
+        Accommodation.objects.filter(id=cid).delete()
     return render(request, 'accomodation.html')
-
-
-#客房订单信息展示
-def show_accomodation(request):
-    people_list = Accommodation.objects.all()
-    return render(request, "accomodation.html", {"people_list": people_list})
-
 
 #客房订单查询
 def search_accomodation(request):
     if request.method == "POST":
-        aid = request.POST.get("id", None)
-        roomid = Accommodation.objects.filter(顾客id=aid)
-        print(roomid)
-        room_id_set = []
-        time = None
-        for f in roomid:
-            rid = f.客房编号
-            room_id_set.append(rid)
-            time = f.入住时间
-        print(room_id_set)
-        room_list = []
-        for room_id in room_id_set:
-            room = Room.objects.get(房间号=room_id)
-            num = room.房间号
-            room_type = room.客房类型
-            price = room.价格
+        cid = request.POST.get("id", None)
+        accommodations = Accommodation.objects.filter(顾客id=cid)
+        filter_list = []
+        for acc in accommodations:
+            room_id = acc.客房编号.房间号
+            time = acc.入住时间
+            room = Room.objects.filter(房间号=room_id).first()
             li = {
-                "房间号": num,
-                "客房类型": room_type,
-                "价格": price,
-                "入住时间": time
+                "房间号": room_id,
+                "客房类型": room.客房类型,
+                "价格": room.价格,
+                "入住时间": time,
             }
-            room_list.append(li)
-        print(room_list)
-    return render(request, 'accomodation.html', {"room_list": room_list})
+            filter_list.append(li)
+    acc_list = Accommodation.objects.all()  
+    return render(request, 'accomodation.html', {"acc_list": acc_list, "filter_list": filter_list})
 
 
 
